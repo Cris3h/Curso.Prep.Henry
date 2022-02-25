4539bf024ae793e67f6a4798ef179d0a1d61c380
cris3h <cris.treachi@gmail.com>
2022-02-25 18:48:17 -0300

    HW finalizada

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -1,20 +1,36 @@
 // Do not change any of the function names
 
+const { default: simpleGit } = require("simple-git");
+
 function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
+  
+  //.replace toma dos valores (a, b) 
+  // a = El que se  quiere cambiar, donde acá, especificamos que indice de la cadena queremos cambiar
+  //b = El valor que queremos. Acá especificamos el indice again y le damos un método para que realice (toUpperCase)
+  //.replace devuelve una nueva lista sin cambiar la original.-
+  //toUppercase va a devolver lo que le especifiquemos en CAPITALS - sí no especifica va por el total de la cadena. (metodo de string)
   //Tu código:
+
+  return nombre.replace(nombre[0], nombre[0].toUpperCase());
 }
+
+
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  //para invocar debemos usar los parentesis
+  cb();
 }
+
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1, n2);
 }
 
 function sumarArray(numeros, cb) {
@@ -22,25 +38,63 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
-}
+  var total = 0
+  for (var i = 0; i < numeros.length; i++){
+    total += numeros[i];
+  }
+  return cb(total);
+  }
+
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
-}
+  // Primera opcion sin usar el método .forEach
+      //for (var i = 0; i < array.length; i++){
+      // cb(array[i]);
+      //}
+      
+      return array.forEach(cb)
+  }
 
 function map(array, cb) {
   // Crea un nuevo array
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  //sin map: 
+  //
+  //var nuevoArr = []; 
+  //for (var i = 0; i < array.length; i++){
+    //  nuevoArr.push(cb(array[i]))
+    //   nuevoArr[i] = cb(array[i])
+    // }
+ // return nuevoArr;
+
+    var newArray = array.map(function (nuevo){
+      return cb(nuevo)
+    })
+    return newArray;
+ 
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  //sin .filter
+ // var nuevo = [];
+ // for (var i = 0; i < array.length; i++){
+  //  if(array[i][0] === 'a'){
+ //     nuevo.push(array[i]);
+//    }
+ // } return nuevo;
+
+    var conA = array.filter(function(empiezaconA){
+      return empiezaconA[0] === 'a';
+    })
+    return conA;
 }
 
 // No modificar nada debajo de esta línea
